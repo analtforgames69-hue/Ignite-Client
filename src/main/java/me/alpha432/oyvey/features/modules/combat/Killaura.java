7ad4d28dbfb8a28d78847e3ca859a38be4cefac7
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-28 22:46:08 +0100

    Update Killaura.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
@@ -1,7 +1,7 @@
 package me.alpha432.oyvey.features.modules.combat;
 
 import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.features.setting.Setting;
+import me.alpha432.oyvey.features.settings.Setting;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Hand;
