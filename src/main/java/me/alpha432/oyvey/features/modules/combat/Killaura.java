3b6a03ed24fbf6280939fba45b0705a70c216436
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-28 22:42:54 +0100

    Update Killaura.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
@@ -1,16 +1,14 @@
 package me.alpha432.oyvey.features.modules.combat;
 
-import com.google.common.eventbus.Subscribe;
-import me.alpha432.oyvey.event.impl.TickEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.setting.Setting;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Hand;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 
 public class Killaura extends Module {
+
     public Setting<Float> range = register(new Setting<>("Range", 4.0f, 1.0f, 6.0f));
     public Setting<Boolean> rotate = register(new Setting<>("Rotate", true));
     public Setting<TargetMode> targetMode = register(new Setting<>("TargetMode", TargetMode.CLOSEST));
@@ -24,15 +22,14 @@ public class Killaura extends Module {
         super("Killaura", "Automatically attacks entities", Category.COMBAT, true, false, false);
     }
 
-    @Subscribe
-    public void onTick(TickEvent.Pre event) {
+    @Override
+    public void onUpdate() {
         Entity target = getTarget();
         if (target != null) {
-            if (rotate.getValue()) {
-                faceEntity(target);
-            }
+            if (rotate.getValue()) faceEntity(target);
 
-            if (mc.player.getAttackCooldownProgress(0) >= 1.0f) { // attack indicator fully recharged
+            // Attack whenever attack cooldown is ready
+            if (mc.player.getAttackCooldownProgress(0) >= 1.0f) {
                 mc.player.attack(target);
                 mc.player.swingHand(Hand.MAIN_HAND);
             }
