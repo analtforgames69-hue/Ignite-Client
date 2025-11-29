e738e412384f00e934985b7fc07059ff84926c53
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-29 12:17:39 +0100

    Update EntityListButton.java

--- src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
@@ -2,7 +2,6 @@ package me.alpha432.oyvey.features.gui.items.buttons;
 
 import me.alpha432.oyvey.features.modules.combat.Killaura;
 import me.alpha432.oyvey.util.render.RenderUtil;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.sound.SoundEvents;
@@ -12,7 +11,6 @@ import java.util.List;
 public class EntityListButton extends Button {
     private final Killaura module;
     private boolean extended = false;
-    private final MinecraftClient mc = MinecraftClient.getInstance();
 
     public EntityListButton(String name, Killaura module) {
         super(name);
@@ -21,38 +19,30 @@ public class EntityListButton extends Button {
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+    public void drawScreen(DrawContext context, int mouseX, int mouseY, float partialTicks) {
 
-        // Draw main button box
-        RenderUtil.rect(
-                context.getMatrices(),
-                this.x,
-                this.y,
-                this.x + this.width,
-                this.y + this.height - 0.5f,
-                this.isHovering(mouseX, mouseY) ? 0x77111111 : 0x55111111
-        );
+        // Draw the main button
+        RenderUtil.rect(context.getMatrices(),
+                this.x, this.y,
+                this.x + (float) this.width,
+                this.y + (float) this.height - 0.5f,
+                this.isHovering(mouseX, mouseY) ? 0x77111111 : 0x55111111);
 
-        // Draw text
-        context.drawText(
-                mc.textRenderer,
-                this.name,
-                (int) (this.x + 2.3f),
-                (int) (this.y + 2),
-                0xFFFFFF,
-                false
-        );
+        drawString(this.getName(), this.x + 2.3f, this.y - 2.0f, -1);
 
-        // Draw extended buttons
+        // Draw extended list
         if (extended) {
+            List<BooleanButton> buttons = module.getEntityButtons();
             float offsetY = this.y + this.height;
 
-            List<BooleanButton> btns = module.getEntityButtons();
-            for (BooleanButton btn : btns) {
-                btn.setX(this.x);
-                btn.setY(offsetY);
-                btn.setWidth(this.width);
-                btn.render(context, mouseX, mouseY, delta);
+            for (BooleanButton btn : buttons) {
+
+                // Button uses direct fields, not setters
+                btn.x = this.x;
+                btn.y = offsetY;
+                btn.width = this.width;
+
+                btn.drawScreen(context, mouseX, mouseY, partialTicks);
 
                 offsetY += btn.getHeight();
             }
@@ -60,33 +50,30 @@ public class EntityListButton extends Button {
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
 
-        if (button == 0 && isHovering(mouseX, mouseY)) {
-            this.extended = !this.extended;
+        if (mouseButton == 0 && this.isHovering(mouseX, mouseY)) {
+            this.extended = !extended;
             mc.getSoundManager().play(
-                    PositionedSoundInstance.create(SoundEvents.UI_BUTTON_CLICK)
+                    PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f)
             );
-            return true;
         }
 
         if (extended) {
             for (BooleanButton btn : module.getEntityButtons()) {
-                btn.mouseClicked(mouseX, mouseY, button);
+                btn.mouseClicked(mouseX, mouseY, mouseButton);
             }
         }
-
-        return false;
     }
 
     @Override
     public int getHeight() {
-        int total = 15;
+        int h = 14;
         if (extended) {
             for (BooleanButton btn : module.getEntityButtons()) {
-                total += btn.getHeight();
+                h += btn.getHeight();
             }
         }
-        return total;
+        return h;
     }
 }
