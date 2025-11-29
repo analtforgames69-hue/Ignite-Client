b428f00202ffb53881d4822a9da3ca66d148e5ba
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-29 12:12:24 +0100

    Update Killaura.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/Killaura.java
@@ -10,30 +10,24 @@ import java.util.List;
 
 public class Killaura extends Module {
 
-    // Settings (replace Setting with your correct Settings class if needed)
     public Setting<Boolean> targetPlayers;
     public Setting<Boolean> targetPhantoms;
 
-    // List of BooleanButtons for the entity list
     private final List<BooleanButton> entityButtons = new ArrayList<>();
-    public EntityListButton entityListButton;
+    public final EntityListButton entityListButton;
 
     public Killaura() {
-        super("Killaura", "Automatically attacks entities around you", Module.Category.COMBAT);
+        super("Killaura", "Automatically attacks entities around you", Category.COMBAT);
 
-        // Initialize settings
         targetPlayers = register(new Setting<>("Players", true));
         targetPhantoms = register(new Setting<>("Phantoms", false));
 
-        // Initialize entity buttons
         entityButtons.add(new BooleanButton("Players", targetPlayers));
         entityButtons.add(new BooleanButton("Phantoms", targetPhantoms));
 
-        // Initialize entity list button
         this.entityListButton = new EntityListButton("Target Entities", this);
     }
 
-    // Getter for entity buttons
     public List<BooleanButton> getEntityButtons() {
         return entityButtons;
     }
