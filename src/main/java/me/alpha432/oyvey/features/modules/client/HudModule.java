f66281448d4753e7e10020b718821bce6bff5022
analtforgames69-hue <analtforgames69@gmail.com>
2025-12-02 17:51:03 +0100

    Update HudModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/client/HudModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/client/HudModule.java
@@ -1,30 +1,32 @@
 package me.alpha432.oyvey.features.modules.client;
 
-import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.event.impl.Render2DEvent;
 import me.alpha432.oyvey.features.modules.Module;
 import net.minecraft.client.MinecraftClient;
-import java.awt.Color;
 
 public class HudModule extends Module {
+
     public HudModule() {
-        super("Hud", "hud", Category.CLIENT, true, false, false);
+        super("Hud", "Displays client HUD", Category.CLIENT, true, false, false);
     }
-@Override
-public void onRender2D(Render2DEvent event) {
-    MinecraftClient mc = MinecraftClient.getInstance();
-    String text = "Ignite v1.0.0.0";
 
-    int screenWidth = event.getContext().getScaledWindowWidth();
-    int textWidth = mc.textRenderer.getWidth(text);
-    int x = screenWidth - textWidth - 2;
-    int y = 2;
+    @Override
+    public void onRender2D(Render2DEvent event) {
+        MinecraftClient mc = MinecraftClient.getInstance();
+        String text = "Ignite v1.0.0.0";
 
-    // Draw plain white text
-    event.getContext().drawTextWithShadow(
-            mc.textRenderer,
-            text,
-            x, y,
-            0xFFFFFFFF   // white
-    );
+        int screenWidth = event.getContext().getScaledWindowWidth();
+        int textWidth = mc.textRenderer.getWidth(text);
+        int x = screenWidth - textWidth - 2;
+        int y = 2;
+
+        // White text
+        event.getContext().drawTextWithShadow(
+                mc.textRenderer,
+                text,
+                x,
+                y,
+                0xFFFFFFFF
+        );
+    }
 }
